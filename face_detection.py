2b1632fd068d0202904a320a5f86e89db8bf0c5f
OsamaAl-Wardi <osamaalwardi@yahoo.com>
2020-02-19 16:54:19 +0100

    comments

--- face_detection.py
+++ face_detection.py
@@ -2,14 +2,14 @@ import numpy as np
 import cv2
 import time
 
-# init
+# Init
 face_cascade_name = 'data/haarcascade_frontalface_default.xml'
 eyes_cascade_name = 'data/haarcascade_eye.xml'
 
 face_cascade = cv2.CascadeClassifier()
 eyes_cascade = cv2.CascadeClassifier()
 
-#-- 1. Load the cascades
+# Loading the cascades
 if not face_cascade.load(cv2.samples.findFile(face_cascade_name)):
     print('--(!)Error loading face cascade')
     exit(0)
@@ -21,14 +21,14 @@ def detectAndDisplay(frame):
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frame_gray = cv2.equalizeHist(frame_gray)
 
-    #-- Detect faces
+    # Detect faces
     faces = face_cascade.detectMultiScale(frame_gray)
     for (x,y,w,h) in faces:
         center = (x + w//2, y + h//2)
         frame = cv2.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
 
         faceROI = frame_gray[y:y+h,x:x+w]
-        #-- In each face, detect eyes
+        # In each face, detect eyes
         eyes = eyes_cascade.detectMultiScale(faceROI)
         for (x2,y2,w2,h2) in eyes:
             eye_center = (x + x2 + w2//2, y + y2 + h2//2)
@@ -42,19 +42,25 @@ def normal_stream():
     frames = 0
     t = time.process_time()
     seconds = 1
+    # Start time measurement
     start = time.time()
     while(True):
         ret, frame = cap.read()        
         frames += 1
+        # Display the frames
         cv2.imshow('frame',frame)
+        # End time measurement
         end = time.time()
         elapsed_time = end - start
+        
         if elapsed_time > seconds:
             print ("Time taken : {0} seconds".format(seconds))
+            # Measuring the frames per second
             fps  = frames / seconds
             print ("Estimated frames per second : {0}".format(fps))
             seconds += 1
 
+        # Exiting the window
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -66,20 +72,24 @@ def face_detection_stream():
     cap = cv2.VideoCapture(0)
     frames = 0
     seconds = 1
+    # Start the time measurement
     start = time.time()
     while(True):
         ret, frame = cap.read()
         frames += 1
+        # Run the cascade face detector
         detectAndDisplay(frame)
+        # End the time measurement
         end = time.time()
         elapsed_time = end - start
         if elapsed_time > seconds:
             print ("Time taken : {0} seconds".format(seconds))
+            # Compute the frames per second
             fps  = frames / seconds
             print ("Estimated frames per second : {0}".format(fps))
             seconds += 1
 
-
+        # Press Q to exit the window
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
